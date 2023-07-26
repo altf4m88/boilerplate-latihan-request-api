b16a67f4e4f62cec90b9bc69195260a5c396a053
altf4m88 <fadhilmauladhani@gmail.com>
2023-07-26 10:31:01 +0700

    boilerplate

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 // // Function to fetch cat data and render the image
-function fetchCatData() {
+function ambilKucing() {
+
     fetch('https://api.thecatapi.com/v1/images/search')
         .then(response => {
             return response.json();
         })
         .then(data => {
+
             const catImageUrl = data[0].url;
-            // renderCatImage(catImageUrl);
+
+            renderKucing(catImageUrl);
         })
         .catch(error => {
         console.error('Fetch error:', error);
         });
-    }
+}
+
+function renderKucing (url) {
+    let element = document.getElementById('cat-image');
+
+    element.src = url;
+}
+
+ambilKucing()
